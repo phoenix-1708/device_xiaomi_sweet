880b9737fb03e7e8d6c306251d6bb27a7ac74ac3
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2023-07-25 10:37:19 +0530

    Update and rename voltage_sweet.mk to lineage_sweet.mk

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -14,11 +14,27 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 # Inherit common Voltage OS  Stuff.
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 2160
-PRODUCT_NAME := voltage_sweet
+RISING_CHIPSET := sm6150
+#RISING_PACKAGE_TYPE := "VANILLA AOSP"
+TARGET_HAS_UDFPS := false
+#TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+# TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_SUPPORTS_QUICK_TAP := true
+# SUSHI_BOOTANIMATION  := 1080
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_CORE_GMS_EXTRAS := true
+RISING_MAINTAINER := Harikumar
+
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-VOLTAGE_BUILD_TYPE := OFFICIAL
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
